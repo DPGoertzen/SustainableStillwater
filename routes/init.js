a55196cac156a95301c89bd9f5a7900c7c883cff
Andrew Kolander <akolander22@gmail.com>
2016-08-18 11:38:05 -0500

    able to add to users initiatives in db

--- routes/init.js
+++ routes/init.js
@@ -5,7 +5,33 @@ var KPI = require('../models/kpi').model;
 
 router.post('/newInit', function(request,response){
   console.log('New Initiative', request.data);
+  var data = request.body;
+  var id = request.user._id;
 
+  var createdInitiative = new Initiative ({
+    pillar: data.pillar,
+    name: data.name
+  })
+  createdInitiative.save(function(err){
+    if(err){
+      console.log(err);
+      response.sendStatus(500);
+    } else {
+      User.findById(id, function(err, user){
+        if(err){
+          console.log(err);
+        };
+
+        user.initiatives.push(createdInitiative);
+        user.save(function(err){
+          if(err){
+            console.log(err);
+          };
+        })
+      })
+      response.sendStatus(200);
+    }
+  })
 });
 
 
