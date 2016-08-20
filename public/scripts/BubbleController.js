2882ea1b97450cfbc89ecae09c5039181e289264
DPGoertzen <dpgoertzen@gmail.com>
2016-08-20 17:33:00 -0500

    bubble sketch fully commented

--- public/scripts/BubbleController.js
+++ public/scripts/BubbleController.js
@@ -7,6 +7,7 @@ angular.module('ssmnApp').controller('BubbleController', ['DataService', '$eleme
   createBubbles();
 
   function createBubbles() {
+    // global variables needed for our sketch
     var leveler = true;
     var width = window.innerWidth;
     var height = window.innerHeight;
@@ -17,14 +18,13 @@ angular.module('ssmnApp').controller('BubbleController', ['DataService', '$eleme
     var currentCircle;
     var ourCircles;
     var ourLines;
-    // var zoom = d3.behavior.zoom()
-    //     .scaleExtent([1, 32])
-    //     .on("zoom", zoomed);
 
+    // create our canvas
     var svg = d3.select("#canvas").append("svg")
     .attr("width", width)
     .attr("height", height)
     .attr("id", "root");
+    // might add these lines back in for zooming later.
     // .append("g")
     // .call(d3.behavior.zoom().scaleExtent([1, 8]).on('zoom', zoomed))
     // .append("g");
@@ -32,15 +32,20 @@ angular.module('ssmnApp').controller('BubbleController', ['DataService', '$eleme
     // function zoomed() {
     //   svg.attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")");
     // }
+
+    // the only way we can set the z-depth with SVG's in D3 is by drawing
+    // the top layer last, so we group things (that's what the 'g' does)
+    // into a back and front layer. FRONT LAYER MUST BE BELOW BACK LAYER!!!
     var layerBack = svg.append('g');
     var layerFront = svg.append('g');
 
-    // var svg = d3.select('svg');
+    // set the middle of the canvas
     var originX = .5*width;
     var originY = .5*height;
     var innerCircleRadius = 125;
-    var outerCircleRadius = 300;
 
+
+    // build our pillar dividers
     var pillarLine1 = layerBack.append("line").attr({
       x1: originX,
       y1: originY,
@@ -69,7 +74,7 @@ angular.module('ssmnApp').controller('BubbleController', ['DataService', '$eleme
     });
 
 
-
+    // build our central circle for the organization.
     var sustainableCircle = layerFront.append("circle").attr({
         class: "originCircle",
         cx: originX,
@@ -80,24 +85,32 @@ angular.module('ssmnApp').controller('BubbleController', ['DataService', '$eleme
         originalScale: innerCircleRadius
     });
 
-
+    // generate our pillars
     arcGenerator(0,120,10, "pink");
     arcGenerator(120,240,20, "orange");
     arcGenerator(240,360,30, "purple");
 
-
+    // specifies where we start, where we end, the distant between orbitters and
+    // what color we want our pillar to be
     function arcGenerator(initialDegree, finalDegree, gapBetweenDegree, color){
+      // since the unit circle starts at 3 o'clock, shift it back
       for(var i = initialDegree - 80; i < finalDegree -100; i+=gapBetweenDegree){
+        // use this to alternate heights
         if(leveler == true){
           leveler = false;
         } else {
           leveler = true;
         }
 
+        // do the hard math (THANKS RYAN MULCAHY) to position our orbitters at a
+        // height based on whether leveler is true or false
         var orbitterX = (originX + ((outerCircleRadius * (leveler ? .9:1.25)) * Math.cos(i*(Math.PI/180))));
         var orbitterY = (originY + ((outerCircleRadius * (leveler ? .9:1.25)) * Math.sin(i*(Math.PI/180))));
 
+        // set our orbitter's radii.
         var orbitRadius = 50;
+        // generate a line from the center of the orbitter to the center of
+        // our origin.
         var orbitterLine = layerBack.append("line").attr({
           x1: orbitterX,
           y1: orbitterY,
@@ -105,6 +118,9 @@ angular.module('ssmnApp').controller('BubbleController', ['DataService', '$eleme
           y2: originY,
           stroke: "black"
         });
+        // create our orbitting circles, storing information about initial
+        // placement so we can retrieve it when we shift them around the
+        // screen
         var orbitter = layerFront.append("circle").attr({
             class: "orbitter",
             cx: orbitterX,
@@ -121,25 +137,29 @@ angular.module('ssmnApp').controller('BubbleController', ['DataService', '$eleme
         });
       }
     }
-    //adding new code for zooming on 8/19. Anything after this is not guarenteed to work.
+    // set up our click handlers for our orbitters and sustainableCircle
     var orbitters = d3.selectAll(".orbitter").on('click', clickedOrbitter);
     var ssCircle = d3.selectAll(".originCircle").on('click', clickedSustainableCircle);
 
     function clickedOrbitter(){
+      // setting up some local vars and initializing some global vars
       currentCircle = this;
       var originCircle = d3.select(".originCircle");
       ourCircles = d3.selectAll("circle");
       ourLines = d3.selectAll("line")
       var newCLine;
       d3.selectAll(".infoRectangle").remove();
+      // If we're not transformed, begin the transformation
       if(!isTransformed){
 
         console.log("isTransformed is currently", isTransformed);
         console.log("we clicked x position", d3.select(this).attr("cx"), "and y position", d3.select(this).attr("cy"));
-        var scaler = 4;
 
-        // console.log("this is our originCircle", originCircle[0][0]);
-        setTimeout(newConnectorLine, 749);
+        // create new connection line just after everything else slots into
+        // place
+        setTimeout(newConnectorLine, 780);
+        // move our currentCircle(the one that was clicked) to the center of
+        // the viewport and make it big. No. BIGGER.
         d3.select(currentCircle).transition()
         .duration(750)
         .attr({
@@ -147,9 +167,8 @@ angular.module('ssmnApp').controller('BubbleController', ['DataService', '$eleme
           cy: originY,
           r: d3.select(currentCircle).attr("r")*3.5
         })
-        // .attr("transform", "translate(" + d3.select(this).attr("originX") + "," + d3.select(this).attr("originY") + ")scale(" + scaler + ")translate(" + -d3.select(this).attr("cx") + "," + -d3.select(this).attr("cy") + ")")
-        // .style("stroke-width", 1.5 / scaler + "px");
-
+        // Do the same to the sustainableCircle, but offset it down and to the
+        // left
         sustainableCircle.transition()
         .duration(750)
         .attr({
@@ -157,24 +176,19 @@ angular.module('ssmnApp').controller('BubbleController', ['DataService', '$eleme
             cy: originY + 200,
             r: sustainableCircle.attr("r")*.3
           })
-        // .attr("transform", "translate(" + -.6*sustainableCircle.attr("cx") + "," + -.6*sustainableCircle.attr("cy") + ")scale(" + .3*scaler + ")")
-        // d3.selectAll("circle").transition
 
 
+        // if ourCircles are the current circle OR it's the central
+        // sustainableCircle, make their opacity full
+        // CHANGE THE SELECTOR ON THE BACK HALF OF THE || TO CLASS, NOT COLOR!!!
         ourCircles.attr({"opacity": function(){
-          // console.log("this is", this)
           return (this === currentCircle || d3.select(this).attr("fill") == "green") ? 1 : 0;
         }});
-
-
+        // drop the extraneous lines to 0 opacity.
         ourLines.attr({"opacity": 0});
 
-
-          // newCLine.transition()
-          // .duration(35)
-          // .attr("transform", "translate(" + d3.select(currentCircle).attr("originX") + "," + d3.select(currentCircle).attr("originY") + ")scale(" + scaler + ")translate(" + -d3.select(currentCircle).attr("cx") + "," + -d3.select(currentCircle).attr("cy") + ")")
-
-          isTransformed = true;
+        isTransformed = true;
+        // if our initiativeRectangle is not displayed, display it.
         }else if(!isIniativeBoxDisplayed){
           var initiativeRectangle = layerFront.append("rect").attr({
             class: "initiativeRectangle",
@@ -188,6 +202,8 @@ angular.module('ssmnApp').controller('BubbleController', ['DataService', '$eleme
             fill: "aqua"
           });
           isIniativeBoxDisplayed = true;
+        // we've clicked currentCircle twice, so get rid of the
+        // initiativeRectangle
         }else{
           d3.selectAll(".initiativeRectangle").remove();
           isIniativeBoxDisplayed = false;
@@ -196,8 +212,12 @@ angular.module('ssmnApp').controller('BubbleController', ['DataService', '$eleme
     }
 
     function clickedSustainableCircle(){
+      //  if isTransformed is true, we know that we're currently zoomed,
+      // therefore we need to unzoom.
       if(isTransformed){
         console.log("isTransformed is currently", isTransformed);
+        // transition to our original position (originX, originY are both the
+        // middle points on our canvas.
         sustainableCircle.transition()
         .duration(750)
         .attr({
@@ -205,24 +225,31 @@ angular.module('ssmnApp').controller('BubbleController', ['DataService', '$eleme
           cy: originY,
           r: sustainableCircle.attr("originalScale")
         });
-
+        // Do the same here -- initialX,Y,R are stored on creation of the
+        // orbitter
         d3.select(currentCircle).transition()
         .duration(750)
         .attr({
           cx: d3.select(currentCircle).attr("initialX"),
           cy: d3.select(currentCircle).attr("initialY"),
           r: d3.select(currentCircle).attr("initialR")
-
         });
+        // delete our connective line
         d3.select(".newCLine").remove();
+        // a helper function to restore our opacity, needs to be a function
+        // because...
         function returnOpacityTo1(){
           ourCircles.attr("opacity", 1);
           ourLines.attr("opacity", 1);
 
         }
+        // ...we're using setTimeout!
         setTimeout(returnOpacityTo1, 600);
+        // if an initiativeRectangle is on screen, delete it.
         d3.selectAll(".initiativeRectangle").remove();
+        // we're no longer transformed!
         isTransformed = false;
+        // if isSCircleClicked is false, we need to create our infoRectangle.
       }else if(!isSCircleClicked){
         console.log("sustainableCircle is clicked and is", isSCircleClicked);
           var infoRectangle = layerFront.append("rect").attr({
@@ -236,7 +263,10 @@ angular.module('ssmnApp').controller('BubbleController', ['DataService', '$eleme
             stroke: "black",
             fill: "aqua"
           });
+        // isSCircleClicked is now true, so set it
         isSCircleClicked = true;
+        // they must've clickedSustainableCircle twice, so get rid of
+        // infoRectangle
       }else{
         console.log("sustainableCircle is clicked and is", isSCircleClicked);
         d3.selectAll(".infoRectangle").remove();
@@ -244,6 +274,8 @@ angular.module('ssmnApp').controller('BubbleController', ['DataService', '$eleme
       }
     }
 
+    // This is how we generate lines when we "zoom" in. needs to be accessible
+    // to both of our "clicked..." functions.
     function newConnectorLine(){
         newCLine = layerBack.append("line").attr({
         class: "newCLine",
@@ -256,7 +288,5 @@ angular.module('ssmnApp').controller('BubbleController', ['DataService', '$eleme
       });
     }
 
-
-      //end new code.
   };  }
 ])
