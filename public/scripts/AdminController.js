4498cb4734ecc19f7a97cfcfb4edf4f7316aacee
DPGoertzen <dpgoertzen@gmail.com>
2016-08-24 09:48:05 -0500

    got admin view working

--- public/scripts/AdminController.js
+++ public/scripts/AdminController.js
@@ -1,16 +1,36 @@
-angular.module('ssmnApp').controller('AdminController', function($http, UserService){
+angular.module('ssmnApp').controller('AdminController', function($http, $mdMedia, $mdDialog, UserService){
   var vm = this;
 
+  vm.getPendingInits = UserService.getPendingInits;
+  vm.getPendingInits();
+  vm.data = UserService.data;
 
-  var pendingPromise = UserService.getPendingInits();
-  pendingPromise.then(function(resultingData){
-    data = resultingData;
-    pendingInits = data.initPendingArray;
-    approvedInits = data.initApprovedArray;
 
-    console.log('Inits pending approval are', data.initPendingArray);
-    console.log('Inits that are approved are', data.initApprovedArray);
+  vm.initModal = function(init) {
+    console.log('ya clicked it');
+    console.log('The initiative is', init);
+    var useFullScreen = ($mdMedia('sm') || $mdMedia('xs'));
+    $mdDialog.show({
+      templateUrl: 'views/initview.html',
+      controller: 'InitViewController',
+      controllerAs: 'initview',
+      fullscreen: useFullScreen,
+      clickOutsideToClose: true,
+      ariaLabel: 'Good',
+      locals: {
+      init: init
+      }
+    })
+  }
+  // var pendingPromise = UserService.getPendingInits();
+  // pendingPromise.then(function(resultingData){
+  //   data = resultingData;
+  //   pendingInits = data.initPendingArray;
+  //   approvedInits = data.initApprovedArray;
 
-  })
+    console.log('Inits pending approval are', vm.data.initPendingArray);
+    console.log('Inits that are approved are', vm.data.initApprovedArray);
+
+  // })
 
 })
