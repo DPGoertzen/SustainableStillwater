3e926c3b4987961cbf64feabd7e25070bb24c530
bqanderson <bqanderson@me.com>
2016-08-26 09:56:31 -0500

    Create phaseform.html -bqa

--- public/scripts/MilestoneController.js
+++ public/scripts/MilestoneController.js
@@ -2,7 +2,17 @@ angular.module('ssmnApp').controller('MilestoneController', function($http){
 
   var vm = this;
 
-    vm.milestones = ["Monetary", "CheckBox", "Number"]
+  vm.milestones = [{id: 'milestone1'}];
 
+  vm.addNewMilestone = function() {
+    console.log('are we here yet?');
+    var newItemNo = vm.milestones.length+1;
+    vm.milestones.push({'id':'milestone'+newItemNo});
+  };
 
-})
+  vm.removeChoice = function() {
+    var lastItem = vm.milestones.length-1;
+    vm.milestones.splice(lastItem);
+  };
+
+});
