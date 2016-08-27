8b570db2ba793ea9eb4991d3286b472ffb99dd52
DPGoertzen <dpgoertzen@gmail.com>
2016-08-27 14:52:30 -0500

    got word wrap working -DG

--- public/scripts/BubbleController.js
+++ public/scripts/BubbleController.js
@@ -38,6 +38,58 @@ angular.module('ssmnApp').controller('BubbleController', ['DataService', '$eleme
     var useFullScreen;
     var orbitRadius = 50;
     var whichPillar = 1;
+
+    d3.util = d3.util || {};
+
+    d3.util.wrap = function(_wrapW, xPos, yPos){
+        return function(d, i){
+            var that = this;
+
+            function tspanify(){
+                var lineH = this.node().getBBox().height;
+                this.text('')
+                    .selectAll('tspan')
+                    .data(lineArray)
+                    .enter().append('tspan')
+                    .attr({
+                        x: xPos,
+                        y: function(d, i){ return (yPos-15) + (i + 1) * lineH; }
+                    })
+                    .text(function(d, i){ return d.join(' '); })
+            }
+
+            function checkW(_text){
+                var textTmp = that
+                    .style({visibility: 'hidden'})
+                    .text(_text);
+                var textW = textTmp.node().getBBox().width;
+                that.style({visibility: 'visible'}).text(text);
+                return textW;
+            }
+
+            var text = this.text();
+            var parentNode = this.node().parentNode;
+            var textSplitted = text.split(' ');
+            var lineArray = [[]];
+            var count = 0;
+            textSplitted.forEach(function(d, i){
+                if(checkW(lineArray[count].concat(d).join(' '), parentNode) >= _wrapW){
+                    count++;
+                    lineArray[count] = [];
+                }
+                lineArray[count].push(d)
+            });
+
+            this.call(tspanify)
+        }
+    };
+
+
+
+
+
+
+
     // create our canvas
     var svg = d3.select("#canvas").append("svg")
     .attr("width", width)
@@ -218,7 +270,8 @@ angular.module('ssmnApp').controller('BubbleController', ['DataService', '$eleme
             initialY: orbitterY,
             initialFontSize: "12px"
           }).style("text-anchor", "middle")
-          .text(currentPillar.array[whichText].name);
+          .text(currentPillar.array[whichText].name)
+          .call(d3.util.wrap(100, orbitterX, orbitterY));
           whichText++;
           whichOrbitter++;
           i+=gapBetweenDegree;
