ef555351959da664606453b9a95659e62a0ee494
Brian Q Anderson <brianqanderson@Elizabeths-MacBook-Pro.local>
2016-09-01 17:27:12 -0500

    Add coloring themes step 1 -bqa

--- public/scripts/InitViewController.js
+++ public/scripts/InitViewController.js
@@ -20,14 +20,16 @@ angular.module('ssmnApp').controller('InitViewController', ['$http', 'init', '$m
 
   var phaseOptions = {
     skin: {
-      type: 'tron'
+      type: 'tron',
+      color: 'rgba(56,142,60,1)'
     },
     size: 200,
     unit: '%',
     barWidth: 40,
-    trackColor: 'rgba(255,0,0,.1)',
-    prevBarColor: 'rgba(0,0,0,.2)',
-    readOnly: true,
+    barColor: 'rgba(56,142,60,.5)',
+    trackColor: 'rgba(56,142,60,.1)',
+    prevBarColor: 'rgba(56,142,60,.2)',
+    readOnly: false,
     fontSize: 30,
     subText: {
       enabled: true,
@@ -48,7 +50,6 @@ angular.module('ssmnApp').controller('InitViewController', ['$http', 'init', '$m
       tempPhaseObject = {
       phaseId: tempPhases[i]._id,
       phaseName: tempPhases[i].label,
-      // phaseValue: tempPhases[i].phaseValue,
       phaseOptions: phaseOptions,
       milestones: []
     }
@@ -62,17 +63,15 @@ angular.module('ssmnApp').controller('InitViewController', ['$http', 'init', '$m
                 skin: {
                   type: 'tron',
                   width: 3,
-                  spaceWidth: 2
+                  spaceWidth: 2,
+                  color: 'rgba(56,142,60,.5)'
                 },
                 size: 100,
-              //  unit: '$',
-              //  max: tempPhases[i].milestones[j].milestoneGoal,
                trackWidth: 20,
                barWidth: 15,
-               trackColor: 'rgba(255,0,0,.1)',
+               barColor: 'rgba(56,142,60,.5)',
+               trackColor: 'rgba(56,142,60,.1)',
                prevBarColor: 'rgba(0,0,0,.2)',
-              //  readOnly: false,
-              //  step: 1, //variable value based on 'unit'
                displayPrevious: true,
 
              }
